3e2bc476e06fc193728d4051f9ef3b6438c91df3
kaisernik1 <nikitakaiser@yandex.ru>
2025-02-09 20:42:50 +0300

    6 try

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Запуск приложения
-go run main.go
+go run main.go &
+SERVER_PID=$! # Сохраняем PID сервера
+
+# Ждём, пока сервер начнёт работу
+sleep 2
+
+# Выводим сообщение об успешном запуске
+echo "Server started in background with PID $SERVER_PID"
