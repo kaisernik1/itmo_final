4122ddf3ae35147884dda6ceadac7d8995a06e73
kaisernik1 <nikitakaiser@yandex.ru>
2025-02-09 22:00:28 +0300

    7 try

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,3 +3,17 @@
 # Установка зависимостей
 go mod init project
 go get github.com/lib/pq
+
+
+# Создание пользователя и базы данных
+psql -U postgres -c "CREATE USER validator WITH PASSWORD 'val1dat0r';"
+psql -U postgres -c "CREATE DATABASE project-sem-1;"
+psql -U postgres -d project-sem-1 -c "
+CREATE TABLE IF NOT EXISTS prices (
+    id TEXT,
+    name TEXT,
+    category TEXT,
+    price INTEGER,
+    create_date DATE
+);
+"
